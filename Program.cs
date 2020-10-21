d8b678141932db3a4323807cb4de315eb08b6919
Gustavo-Travaioli <73239484+Gustavo-Travaioli@users.noreply.github.com>
2020-10-21 11:30:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoLL.View;
+
+namespace ProjetoLL
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+           // Application.Run(new FrmProjeto());
+            //Application.Run(new FrmPrincipal());
+           Application.Run(new FrmLogin());
+        }
+    }
+}
